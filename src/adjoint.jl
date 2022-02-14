430306a3db12d1689cbc96bffe22aa3989e18be8
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2022-02-14 13:51:56 -0700

    jump function defined at discrete times

--- src/adjoint.jl
+++ src/adjoint.jl
@@ -1,5 +1,7 @@
-function discrete_adjoint(sol, g;
-    abstol=1e-10, reltol=1e-10)
+function discrete_adjoint(sol, g, t; abstol=1e-10, reltol=1e-10)
+
+    @assert sol.dense "The solution must be dense"
+    @assert all(tidx -> tidx in sol.t, t) "State variables must be defined at every data point"
 
     @unpack f, p, u0, tspan = sol.prob
 
@@ -14,19 +16,30 @@ function discrete_adjoint(sol, g;
     r_u(u, p, t, uprev, tprev) = ForwardDiff.jacobian((u)->r(u, p, t, uprev, tprev), u)
     r_uprev(u, p, t, uprev, tprev) = ForwardDiff.jacobian((uprev)->r(u, p, t, uprev, tprev), uprev)
     r_p(u, p, t, uprev, tprev) = ForwardDiff.jacobian((p)->r(u, p, t, uprev, tprev), p)
-    g_u(u,p) = transpose(ForwardDiff.gradient((u)->g(u,p), u))
+    g_u(u, p, t, i) = ForwardDiff.gradient((u)->g(u, p, t, i), u)
+    g_p(u, p, t, i) = ForwardDiff.gradient((p)->g(u, p, t, i), p)
 
     # Discrete Adjoint Solution
     dp = zeros(length(p))
     tmp = zeros(length(u0))
     for i = length(sol):-1:2
-        u, uprev = sol.u[i], sol.u[i-1]
-        t, tprev = sol.t[i], sol.t[i-1]
-        λ = r_u(u,p,t,uprev,tprev)' \ (g_u(u,p)' - tmp)
-        dp -= r_p(u,p,t,uprev,tprev)'*λ
-        tmp = r_uprev(u,p,t,uprev,tprev)'*λ
+        ui, uprev = sol.u[i], sol.u[i-1]
+        ti, tprev = sol.t[i], sol.t[i-1]
+        idx = findfirst(tidx -> tidx == ti, t)
+        if isnothing(idx)
+            λ = r_u(ui,p,ti,uprev,tprev)' \ (-tmp)
+            dp .-= r_p(ui,p,ti,uprev,tprev)'*λ
+            tmp .= r_uprev(ui,p,ti,uprev,tprev)'*λ
+        else
+            λ = r_u(ui,p,ti,uprev,tprev)' \ (g_u(ui, p, ti, idx) - tmp)
+            dp .+= g_p(ui, p, ti, idx) - r_p(ui,p,ti,uprev,tprev)'*λ
+            tmp .= r_uprev(ui,p,ti,uprev,tprev)'*λ
+        end
     end
-    du0 = g_u(u0,p)' - tmp
+    idx = findfirst(tidx -> tidx == tspan[1], t)
+    if !isnothing(idx)
+        du0 = g_u(u0, p, tspan[1], idx) - tmp
+    end
 
     return dp, du0
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,27 +4,27 @@ using Test
 @testset "DiscreteAdjoint.jl" begin
     
     # ODE problem
-    function f(du,u,p,t)
+    function f(du, u, p, t)
         du[1] = dx = p[1]*u[1] - p[2]*u[1]*u[2]
         du[2] = dy = -p[3]*u[2] + p[4]*u[1]*u[2]
     end
     p = [1.5,1.0,3.0,1.0]; tspan = (0.0, 10.0); u0 = [1.0;1.0]; 
-    prob = ODEProblem(f,u0,tspan,p)
-    sol = solve(prob,Tsit5(),u0=u0,p=p,saveat=0.1, tstops=0.1:0.1:10.0, 
-        abstol=1e-10, reltol=1e-10)
+    prob = ODEProblem(f, u0, tspan, p)
+    sol = solve(prob, Tsit5(), u0=u0, p=p, abstol=1e-10, reltol=1e-10, tstops=0:0.1:10.0)
     
     # jump function
-    g(u,p) = sum(u)
+    g(u,p,t,i) = sum(u)
+    t = 0:0.1:10.0
     
     # discrete adjoint solution
-    dp, du0 = discrete_adjoint(sol, g; abstol=1e-10, reltol=1e-10)
+    dp, du0 = discrete_adjoint(sol, g, t; abstol=1e-10, reltol=1e-10)
     # dp = [8.30536  -159.484  75.2035  -339.195]
     # du0 = [-39.1275  -8.78778]
 
     # forward AD solution
     function sum_of_solution(x)
-        _prob = remake(prob,u0=x[1:2],p=x[3:end])
-        sum(solve(_prob,Tsit5(),saveat=0.1,tstops=0.1:0.1:10.0, abstol=1e-10, reltol=1e-10))
+        _prob = remake(prob, u0=x[1:2], p=x[3:end])
+        sum(solve(_prob, Tsit5(), abstol=1e-10, reltol=1e-10, saveat=0.1))
     end
 
     dx = ForwardDiff.gradient(sum_of_solution,[u0;p])
