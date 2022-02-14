b509b2bb43459b84b0ace99d9281639da7306ddb
Documenter.jl <documenter@juliadocs.github.io>
2022-02-14 17:56:35 +0000

    build based on ef70a75

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DiscreteAdjoint","category":"page"},{"location":"#DiscreteAdjoint","page":"Home","title":"DiscreteAdjoint","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DiscreteAdjoint.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DiscreteAdjoint]","category":"page"}]
+}
