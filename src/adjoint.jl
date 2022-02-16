3b83b681811f4d75a57173ca592d87bee25d4ef8
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2022-02-16 09:32:04 -0700

    use vector jacobian products

--- src/adjoint.jl
+++ src/adjoint.jl
@@ -1,94 +1,65 @@
 function discrete_adjoint(sol, dg, t; kwargs...)
 
-    @assert sol.dense "The solution must be dense"
-    @assert all(tidx -> tidx in sol.t, t) "State variables must be defined at every data point"
+    @assert sol.dense "Currently `discrete_adjoint` only works with dense solutions"
+    @assert all(tidx -> tidx in sol.t, t) "A `tstop` must be set for every data point"
 
     @unpack prob, alg = sol
     @unpack f, p, u0, tspan = prob
     t0, tf = tspan
    
-    # residual function
     integrator = OrdinaryDiffEq.init(prob, alg; kwargs...)
-    fru_cache = dualcache(integrator.cache, ForwardDiff.pickchunksize(length(u0)))
-    fru = (resid, t, dt, uprev, u, p) -> step_residual!(resid, t, dt, uprev, u, f, p, fru_cache)
-    frp_cache = dualcache(integrator.cache, ForwardDiff.pickchunksize(length(p)))
-    frp = (resid, t, dt, uprev, u, p) -> step_residual!(resid, t, dt, uprev, u, f, p, frp_cache)
-        
-    # residual previous state jacobian
-    fr_uprev = PreviousStateJacobianWrapper(fru, t0, t0, u0, p)
-    fr_uprev_config = ForwardDiff.JacobianConfig(fr_uprev, u0, u0)
-    Jr_uprev = (Juprev, resid, t, dt, uprev, u, p) -> begin
-        fr_uprev.t = t
-        fr_uprev.dt = dt
-        fr_uprev.u = u
-        fr_uprev.p = p
-        ForwardDiff.jacobian!(Juprev, fr_uprev, resid, uprev, fr_uprev_config)
-        return Juprev
-    end
 
-    # residual current state jacobian
-    fr_u = CurrentStateJacobianWrapper(fru, t0, t0, u0, p)
-    fr_u_config = ForwardDiff.JacobianConfig(fr_u, u0, u0)
-    Jr_u = (Ju, resid, t, dt, uprev, u, p) -> begin
-        fr_u.t = t
-        fr_u.dt = dt
-        fr_u.uprev = uprev
-        fr_u.p = p
-        ForwardDiff.jacobian!(Ju, fr_u, resid, u, fr_u_config)
-        return Ju
-    end
+    # current state jacobian function
+    jac = state_jacobian(integrator)
 
-    # residual parameter jacobian
-    fr_p = ParamJacobianWrapper(frp, t0, t0, u0, u0)
-    fr_p_config = ForwardDiff.JacobianConfig(fr_p, u0, p)
-    Jr_p = (Jp, resid, t, dt, uprev, u, p) -> begin
-        fr_p.t = t
-        fr_p.dt = dt
-        fr_p.u = u
-        fr_p.uprev = uprev
-        ForwardDiff.jacobian!(Jp, fr_p, resid, p, fr_p_config)
-        return Jp
-    end
+    # previous state vector-jacobian product function
+    uvjp = previous_state_jacobian_product(integrator)
 
-    # output state gradient
-    g_u = (gu, u, p, t, i) -> begin
-        dg(gu, u, p, t, i)
-        return gu
-    end
+    # parameter vector-jacobian product function
+    pvjp = parameter_jacobian_product(integrator)
 
-    # pre-allocated storage
-    resid = zeros(length(u0))
-    Ju = zeros(length(u0), length(u0))
-    Juprev = zeros(length(u0), length(u0))
-    Jp = zeros(length(u0), length(p))
-    gu = zeros(length(u0))
- 
     # Discrete Adjoint Solution
     dp = zeros(length(p))
     du0 = zeros(length(u0))
     λ = zeros(length(u0))
-    tmp = zeros(length(u0))
+    rhs = zeros(length(u0))
+    resid = zeros(length(u0))
+    J = zeros(length(u0), length(u0))
+    dgval = zeros(length(u0))
+    uvjpval = zeros(length(u0))
+    pvjpval = zeros(length(p))
     for i = length(sol):-1:2
+        # arguments for this step
         ti = sol.t[i]
         dt = ti - sol.t[i-1]
         uprev = sol.u[i-1]
         ui = sol.u[i]
+        # compute right hand side
         idx = findfirst(tidx -> tidx == ti, t)
         if !isnothing(idx)
-            tmp .-= g_u(gu, ui, p, ti, idx)
+            dg(dgval, ui, p, ti, idx)
+            rhs .-= dgval
         end
-        tmp .*= -1
-        if OrdinaryDiffEq.isimplicit(sol.alg)
-            λ .= Jr_u(Ju, resid, ti, dt, uprev, ui, p)' \ tmp
+        rhs .*= -1
+        # compute adjoint vector
+        if OrdinaryDiffEq.isimplicit(alg)
+            jac(J, resid, ti, dt, uprev, ui, p)
+            λ .= J' \ rhs
         else
-            λ .= tmp
+            λ .= rhs
         end
-        mul!(dp, Jr_p(Jp, resid, ti, dt, uprev, ui, p)', λ, -1, 1)
-        mul!(tmp, Jr_uprev(Juprev, resid, ti, dt, uprev, ui, p)', λ)
-    end   
-    idx = findfirst(tidx -> tidx == tspan[1], t)
+        # accumulate gradient
+        pvjp(pvjpval, λ, ti, dt, uprev, ui, p)
+        dp .-= pvjpval
+        # initialize right hand side for the next iteration
+        uvjp(uvjpval, λ, ti, dt, uprev, ui, p)
+        rhs .= uvjpval
+    end
+    # define gradient with respect to the inputs
+    idx = findfirst(tidx -> tidx == t0, t)
     if !isnothing(idx)
-        du0 .= g_u(gu, u0, p, tspan[1], idx) .- tmp
+        dg(dgval, u0, p, t0, idx)
+        du0 .= dgval .- rhs
     end
 
     return dp, du0
--- src/derivatives.jl
+++ src/derivatives.jl
@@ -14,7 +14,34 @@ function (ff::CurrentStateJacobianWrapper)(u)
     return resid
 end
 
-mutable struct PreviousStateJacobianWrapper{F,T,U,P} <: Function
+function state_jacobian(integrator)
+
+    @unpack prob, alg = integrator.sol
+    @unpack f, p, u0, tspan = prob
+    t0, tf = tspan
+
+    fucache = dualcache(integrator.cache, ForwardDiff.pickchunksize(length(u0)))
+    
+    fr = (resid, t, dt, uprev, u, p) -> step_residual!(resid, t, dt, uprev, u, f, p, fucache)
+
+    fru = CurrentStateJacobianWrapper(fr, t0, t0, u0, p)
+    
+    cfg = ForwardDiff.JacobianConfig(fru, u0, u0)
+    
+    jac = (J, resid, t, dt, uprev, u, p) -> begin
+        fru.t = t
+        fru.dt = dt
+        fru.uprev = uprev
+        fru.p = p
+        ForwardDiff.jacobian!(J, fru, resid, u, cfg)
+        return J
+    end
+
+    return jac
+end
+
+mutable struct PreviousStateJacobianProductWrapper{F,T,U,P} <: Function
+    λ::U
     f::F
     t::T
     dt::T
@@ -22,15 +49,42 @@ mutable struct PreviousStateJacobianWrapper{F,T,U,P} <: Function
     p::P
 end
 
-(ff::PreviousStateJacobianWrapper)(resid, uprev) = ff.f(resid, ff.t, ff.dt, uprev, ff.u, ff.p)
+(ff::PreviousStateJacobianProductWrapper)(uprev) = ff.f(ff.λ, ff.t, ff.dt, uprev, ff.u, ff.p)
 
-function (ff::PreviousStateJacobianWrapper)(uprev)
-    resid = similar(uprev)
-    ff.f(resid, ff.t, ff.dt, uprev, ff.u, ff.p)
-    return resid
+function previous_state_jacobian_product(integrator)
+
+    @unpack prob, alg = integrator.sol
+    @unpack f, p, u0, tspan = prob
+    t0, tf = tspan
+
+    N = ForwardDiff.pickchunksize(length(u0))
+    fcache = dualcache(integrator.cache, N)
+    rcache = PreallocationTools.dualcache(u0, N) 
+
+    fvjp = (λ, t, dt, uprev, u, p) -> begin 
+        resid = PreallocationTools.get_tmp(rcache, uprev)
+        step_residual!(resid, t, dt, uprev, u, f, p, fcache)
+        return λ'*resid
+    end
+
+    fruprev = PreviousStateJacobianProductWrapper(u0, fvjp, t0, t0, u0, p)
+    
+    cfg = ForwardDiff.GradientConfig(fruprev, u0)
+    
+    uvjp = (uvjpval, λ, t, dt, uprev, u, p) -> begin
+        fruprev.λ = λ
+        fruprev.t = t
+        fruprev.dt = dt
+        fruprev.u = u
+        fruprev.p = p
+        ForwardDiff.gradient!(uvjpval, fruprev, uprev, cfg)
+    end
+
+    return uvjp
 end
 
-mutable struct ParamJacobianWrapper{F,T,U} <: Function
+mutable struct ParamJacobianProductWrapper{F,T,U} <: Function
+    λ::U
     f::F
     t::T
     dt::T
@@ -38,10 +92,36 @@ mutable struct ParamJacobianWrapper{F,T,U} <: Function
     u::U
 end
 
-(ff::ParamJacobianWrapper)(resid,p) = ff.f(resid, ff.t, ff.dt, ff.uprev, ff.u, p)
+(ff::ParamJacobianProductWrapper)(p) = ff.f(ff.λ, ff.t, ff.dt, ff.uprev, ff.u, p)
 
-function (ff::ParamJacobianWrapper)(p)
-    resid = similar(p, size(ff.u))
-    ff.f(resid, ff.t, ff.dt, ff.uprev, ff.u, p)
-    return resid
+function parameter_jacobian_product(integrator)
+
+    @unpack prob, alg = integrator.sol
+    @unpack f, p, u0, tspan = prob
+    t0, tf = tspan
+
+    N = ForwardDiff.pickchunksize(length(p))
+    fcache = dualcache(integrator.cache, N)
+    rcache = PreallocationTools.dualcache(u0, N) 
+
+    fvjp = (λ, t, dt, uprev, u, p) -> begin 
+        resid = PreallocationTools.get_tmp(rcache, p)
+        step_residual!(resid, t, dt, uprev, u, f, p, fcache)
+        return λ'*resid
+    end
+
+    frp = ParamJacobianProductWrapper(u0, fvjp, t0, t0, u0, u0)
+    
+    cfg = ForwardDiff.GradientConfig(frp, p)
+    
+    pvjp = (pvjpval, λ, t, dt, uprev, u, p) -> begin
+        frp.λ = λ
+        frp.t = t
+        frp.dt = dt
+        frp.uprev = uprev
+        frp.u = u
+        ForwardDiff.gradient!(pvjpval, frp, p, cfg)
+    end
+
+    return pvjp   
 end
