4bb6a527d5d64611a79f8a509d7b56c3608cd4ff
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2022-08-02 10:20:08 -0600

    add DP5

--- src/caches/low_order_rk_caches.jl
+++ src/caches/low_order_rk_caches.jl
@@ -2,5 +2,12 @@ temporary_variables(cache::Tsit5ConstantCache) = (;)
 
 function temporary_variables(cache::Tsit5Cache)
     @unpack k1, k2, k3, k4, k5, k6, tmp = cache
-    return (;k1, k2, k3, k4, k5, k6, tmp)
+    return (; k1, k2, k3, k4, k5, k6, tmp)
+end
+
+temporary_variables(cache::DP5ConstantCache) = (;)
+
+function temporary_variables(cache::DP5Cache)
+    @unpack k1, k2, k3, k4, k5, k6, tmp = cache
+    return (; k1, k2, k3, k4, k5, k6, tmp)
 end
